0e6f40a7a7aeb52c5aba955a762b084ce60938d3
Jackie Jin <liveaeon@gmail.com>
2014-04-12 14:04:12 -0700

    fb data in progress

--- public/js/FBLogin.js
+++ public/js/FBLogin.js
@@ -1,24 +1,66 @@
-//connect to goinstant
-var url = "https://goinstant.net/469216b0e2ee/Flint_Facebook_Login_Test";
-var connect = goinstant.connect(url);
+$(document).ready(function() {
 
-var conn;
-var room;
-var user;
+    var firebaseDataReference = new Firebase('https://flint.firebaseio.com/');
 
-//connect to the database
-var userPoolKey;
-connect.then(function(result) {
-    conn = result.connection;
-    room = result.rooms[0];
-    userPoolKey = room.key('data');
-    return room.self().get();
-}).then(function() {
-    var loginButton = document.getElementById("FacebookLogin");
+    var auth = new FirebaseSimpleLogin(firebaseDataReference, function(error, user) {
+        if (error) {
+            // an error occurred while attempting login
+            console.log(error);
+        } else if (user) {
+            // user authenticated with Firebase
+            //console.log(user);
+            //facebook api
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '1404685476473865',
+                    status     : true,
+                    xfbml      : true
+                });
+                
+                var accessToken = user.accessToken;
+                console.log("access token:" + accessToken);
+                
+                var FBData = {
+                    facebookId: user.id
+                };
+                //return FB data in promises
+                var userStr = '/'+user.id;
+                FB.api(userStr+'/name','GET',{access_token: accessToken},function(response) {
+                    console.log("response generated!");
+                    console.log(response);
+                    //get name 
+                    
+                });
+                
+            };
+            
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/all.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            
+        } else {
+            // user is logged out
+            console.log('User is logged out');
+        }
+    });
+
     
+    var loginButton = document.getElementById("FacebookLogin");
     loginButton.onclick = function() {
-        //login through facebook
-        var loginURL = conn.loginUrl('facebook');
-        console.log(loginURL);
-    }
+
+        auth.login('facebook', {
+            scope:"user_interests,user_likes,email,user_location,user_about_me,user_hometown,user_photos"
+        });
+    };
+
+    var logoutButton = document.getElementById("FacebookLogout");
+    logoutButton.onclick = function() {
+        auth.logout();
+    };
+
 });
+

8ac6b7b3fe8ee5a02e4cff91e52ad3250752fc24
rtyqwe <ryanwmchan@icloud.com>
2014-04-13 03:54:17 -0700

    fix ident

--- routes/initIsaacloud.js
+++ routes/initIsaacloud.js
@@ -13,37 +13,38 @@ exports.initIsaacloud = function (req, res) {
             'Authorization': 'Basic NjE6YzVlMzMwOWVjNGEyNzIzZDUzZjhjNmExODVlMmMz'
         }
   };
-  curl.request(options, function (error, response, body) {
-    if(!error) {
-        accessToken = JSON.parse(response).access_token;
-        console.log(accessToken);
-    } 
-    else {
-      console.log("Error");
-    }
-    if(!userEmail) userEmail = "1ryanwmcan@ahoo.com";
-    curl.request({
-      method: "POST",
-      url: "https://api.isaacloud.com/v1/admin/users",
-      data: JSON.stringify({"email": userEmail}),
-      headers: 
-      {
-        'Authorization': 'Bearer '+ accessToken,
-        'Content-Type': 'application/json;UTF-8'
-      }
-    }, function (error, response, body) {
-      if (JSON.parse(response).id == null){
-        console.log("User has already been created.");
-        res.json(-1);
-      }
-      else if(!error) {
-        userID= JSON.parse(response).id;
-        console.log(userID);
-        res.json(userID);
-      }
-      else {
-        console.log("Error");
-      }
+    curl.request(options, function (error, response, body) {
+        if(!error) {
+            accessToken = JSON.parse(response).access_token;
+            console.log(accessToken);
+        } 
+        else {
+            console.log("Error");
+        }
+        if(!userEmail) userEmail = "1ryanwmcan@ahoo.com";
+        curl.request({
+            method: "POST",
+            url: "https://api.isaacloud.com/v1/admin/users",
+            data: JSON.stringify({"email": userEmail}),
+            headers: 
+            {
+                'Authorization': 'Bearer '+ accessToken,
+                'Content-Type': 'application/json;UTF-8'
+            }
+        }, function (error, response, body) {
+            if (JSON.stringify(response).id == null){
+                console.log("User has already been created.");
+                res.json(-1);
+            }
+            else if(!error) {
+                userID= JSON.parse(response).id;
+                console.log(userID);
+                res.json(userID);
+            }
+            else {
+                console.log("Error");
+            }
+        });
     });
   });
 }

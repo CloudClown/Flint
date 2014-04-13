50e6953142f7e915623f31900f2130d898cfc9c2
Jackie Jin <liveaeon@gmail.com>
2014-04-13 08:28:15 -0700

    twilio integraton in progress

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
  * Module dependencies.
  */
 
- var express = require('express');
- var routes = require('./routes');
- var getLevel = require('./routes/getLevel');
- var initIsaacloud = require('./routes/initIsaacloud');
- var http = require('http');
- var path = require('path');
- var sass = require('node-sass');
- var app = express();
+var express = require('express');
+var routes = require('./routes');
+var getLevel = require('./routes/getLevel');
+var initIsaacloud = require('./routes/initIsaacloud');
+var http = require('http');
+var path = require('path');
+var sass = require('node-sass');
+var app = express();
+
+var flintTwilio = require('./routes/flint-twilio');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,19 +27,19 @@ app.use(express.session());
 app.use(app.router);
 
 app.use(
-  sass.middleware({
-            src: __dirname + '/public', //where the sass files are
-            dest: __dirname + '/public', //where css should go
-            debug: true // obvious
-          })
-  );
+    sass.middleware({
+        src: __dirname + '/public', //where the sass files are
+        dest: __dirname + '/public', //where css should go
+        debug: true // obvious
+    })
+);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-  app.locals.pretty = true;
+    app.use(express.errorHandler());
+    app.locals.pretty = true;
 }
 
 app.get('/', routes.index);
@@ -48,6 +50,8 @@ app.post('/isaacloud/init', initIsaacloud.initIsaacloud);
 app.get('/match', routes.match);
 app.get('/chat', routes.chat);
 
+app.post('./twilio', flint-twilio.flintTwilio);
+
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });

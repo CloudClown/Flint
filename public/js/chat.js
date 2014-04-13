6a1560570d8e6b7b562f1622045ef774101ec8ef
Robert Nguyen <roberttnb@gmail.com>
2014-04-13 05:22:34 -0700

    Added room functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,38 +1,117 @@
-var chatApp = angular.module('Chat', ['goangular', 'ngAnimate', 'angularSlideables']);
+var chatApp = angular.module('Chat', ['goangular', 'ngAnimate', 'angularSlideables', 'firebase']);
+
+function getParamByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
 chatApp.config(function($goConnectionProvider) {
   $goConnectionProvider.$set('https://goinstant.net/469216b0e2ee/Flint');
 });
 var test;
-chatApp.controller('ChatCtrl', function($scope, $goKey) {
+
+chatApp.controller('ChatCtrl', function($scope, $goKey, $firebase, $firebaseSimpleLogin) {
+  var room = getParamByName('room');
+
+  $scope.interests = [];
+  var ref = new Firebase("https://flint.firebaseio.com/");
+  $scope.auth = $firebaseSimpleLogin(ref);
+  $scope.auth.$getCurrentUser().then(function(user) {
+    var mateId;
+    $scope.mateId = $goKey('accounts/' + user.id + '/matches/' + room + '/mateId');
+    $scope.mateId.$sync();
+    $scope.mateId.$on('ready', function() {
+      mateId = $scope.mateId.$value;
+      console.log(mateId)
+
+    $scope.movies = $goKey('accounts/'+mateId+'/movies');
+    $scope.movies.$sync();
+    $scope.movies.$on('ready', function() {
+      $scope.interests.push({
+        title: "Movies",
+        items: [
+            $scope.movies[0],
+            $scope.movies[1],
+            $scope.movies[2]
+        ],
+      });
+    });
+    
+    $scope.music = $goKey('accounts/' + mateId+ '/music');
+    $scope.music.$sync();
+    $scope.music.$on('ready', function() {
+      $scope.interests.push({  
+        title: "Music",
+        items: [
+          $scope.music[0],
+          $scope.music[1],
+          $scope.music[2]
+        ]
+      });
+    });
+ 
+    $scope.tv = $goKey('accounts/' + mateId+ '/television');
+    $scope.tv.$sync();
+    $scope.tv.$on('ready', function() {
+      $scope.interests.push({  
+        title: "TV Shows",
+        items: [
+          $scope.tv[0],
+          $scope.tv[1],
+          $scope.tv[2]
+        ]
+      });
+    });
+
+    $scope.languages = $goKey('accounts/' + mateId+ '/languages');
+    $scope.languages.$sync();
+    $scope.languages.$on('ready', function() {
+      $scope.interests.push({  
+        title: "Languages",
+        items: [
+          $scope.languages[0],
+          $scope.languages[1],
+          $scope.languages[2]
+        ]
+      });
+    });
+
+    $scope.hometown = $goKey('accounts/' + mateId + '/hometown');
+    $scope.hometown.$sync();
+    $scope.hometown.$on('ready', function( ) { 
+      $scope.interests.push({
+        title: "Hometown",
+        items: [
+          $scope.hometown.$value
+        ]
+      });
+    });
+
+    $scope.likes = $goKey('accounts/' + mateId + '/likes');
+    $scope.likes.$sync();
+    $scope.likes.$on('ready', function( ) { 
+      $scope.interests.push({
+        title: "Likes",
+        items: [
+          $scope.likes[0].name,
+          $scope.likes[1].name,
+          $scope.likes[2].name,
+        ]
+      });
+    });
+
+    });
+
+  });
+
+
   $scope.messages = $goKey('messages');
   $scope.messages.$sync();
 
   $scope.user = $goKey('accounts/')
 
-  $scope.interests = [
-    {
-      title: "TV Shows",
-      items: [
-        "Mad Men", 
-        "Game Of Thrones"
-      ]
-    },
-    {
-      title: "Music",
-      items: [
-        "Nirvana", 
-        "Miley Cyrus"
-      ]
-    },
-    {
-      title: "Languages",
-      items: [
-        "English", 
-        "Chinese"
-      ]
-    }
-  ];
 
   $scope.notifications = [
   {

70c05331ecf974f56de6f5f14eb41d717031960d
rtyqwe <ryanwmchan@icloud.com>
2014-04-13 04:24:30 -0700

    fixed json.parse(response).id

--- routes/getLevel.js
+++ routes/getLevel.js
@@ -2,6 +2,7 @@ var curl = require('curlrequest');
 
 exports.getLevel = function(req, res) {
   var userID = req.query.userID;
+  var roomID = req.query.roomID;
   var accessToken;
   var options = {
         method: 'POST',
@@ -30,8 +31,8 @@ exports.getLevel = function(req, res) {
       }
     }, function (error, response, body) {
       if (!error) {
-        var points = JSON.parse(response).counterValues;
-        var ppoint = points[0].value;
+        var all_points = JSON.parse(response).customFields;
+        var ppoint = all_points.roomID;
         if(ppoint > 200)
           res.json(10);
         else if(ppoint > 160)
@@ -60,3 +61,43 @@ exports.getLevel = function(req, res) {
 
   });
 }
+
+exports.updatePoints = function(req, res) {
+  var userID = req.query.userID;
+  var roomID = req.query.roomID;
+  var newpoints = req.query.newPoints;
+  var accessToken;
+  var options = {
+        method: 'POST',
+        url: 'https://oauth.isaacloud.com/token',
+        data: { grant_type:'client_credentials'},
+        headers: {
+            'Authorization': 'Basic NjE6YzVlMzMwOWVjNGEyNzIzZDUzZjhjNmExODVlMmMz'
+        }
+  };
+  curl.request(options, function (error, response, body) {
+    if(!error) {
+        accessToken = JSON.parse(response).access_token;
+        console.log(accessToken);
+    } 
+    else {
+      console.log("Error");
+    }
+
+    if(!userID) userID =1;
+    curl.request({
+      method: 'POST',
+      url: 'https://api.isaacloud.com/v1/cache/users/' + userID,
+      data: {customFields:{ roomID: newPoints }},
+      headers: 
+      {
+        'Authorization': 'Bearer '+ accessToken
+      }
+    }, function (error, response, body) {
+      if (!error) {
+       
+    });
+
+  });
+}
+

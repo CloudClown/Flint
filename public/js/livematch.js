b2a9dd3bef0dfa010e299eb8e3efef1bfe2eb223
Robert Nguyen <roberttnb@gmail.com>
2014-04-14 20:50:03 -0700

    Fixed matchmaking bugs

--- public/js/livematch.js
+++ public/js/livematch.js
@@ -6,23 +6,35 @@ matchApp.config(function($goConnectionProvider) {
 
 matchApp.controller('MatchCtrl', function($scope, $goKey, $firebase, $firebaseSimpleLogin, $goUsers, $goConnection, $window) {
 
+  console.log("active");
   var ref = new Firebase("https://flint.firebaseio.com/");
   $scope.auth = $firebaseSimpleLogin(ref);
   $scope.auth.$getCurrentUser().then(function(user) {
+    console.log(user.id)
     $scope.user = $goKey('users/' + user.id);
-    $scope.user.$sync();
-    $scope.user.$on('ready', function() {
-      $scope.user.$set("active");
-      console.log("User is active!");
+  $scope.user.$sync();
+  $scope.user.$on('ready', function() {
+    $scope.user.$set("active");
+    console.log("User is active!");
+  });
+  $window.onbeforeunload = function() {
+    $scope.user.$remove().then(function ( ) {
+      console.log("User Removed!");
     });
-    $window.onbeforeunload = function() {
-      $scope.user.$remove().then(function ( ) {
-        console.log("User Removed!");
-      });
-    };
+  };
   });
 
   $scope.auth.$getCurrentUser().then(function(user) {
+    $scope.matched = $goKey('accounts/' + user.id + '/matched');
+    $scope.matched.$sync();
+    $scope.matched.$on('ready', function() {
+      console.log($scope.matched);
+      if ($scope.matched.$value) {
+        var newRoom = $scope.matched.$value;
+        $scope.matched.$remove();
+        document.location.href = "/chat?room=" + newRoom;
+      }
+    });
     $scope.users = $goKey('users');
     $scope.users.$sync();
     $scope.users.$on("ready", function() {
@@ -32,28 +44,36 @@ matchApp.controller('MatchCtrl', function($scope, $goKey, $firebase, $firebaseSi
       $scope.counter.$on('ready', function() {
         // Pick Random Active User
         $scope.users.$$key.get(function (err, result) {
-          var keys = Object.keys(result)
+          var keys = Object.keys(result);
           keys.splice(keys.indexOf(user.id), 1);
-        $scope.mateId = keys[ keys.length * Math.random() << 0];
-        });
-        var room = $scope.counter.$value + 1;
-        $scope.counter.$set(room);
+          if(keys.length > 0) {
+            $scope.mateId = keys[ keys.length * Math.random() << 0];
+            var room = $scope.counter.$value + 1;
+            $scope.counter.$set(room);
 
-        $scope.userAcc = $goKey('accounts/' + user.id + '/matches/' + room + '/mateId');
-        $scope.userAcc.$sync();
-        $scope.userAcc.$on("ready", function() {
-          $scope.userAcc.$set($scope.mateId).then(function() {
-
+            $scope.userAcc = $goKey('accounts/' + user.id + '/matches/' + room + '/mateId');
+            $scope.userAcc.$sync();
+            $scope.mateMatched = $goKey('accounts/' + $scope.mateId + '/matched');
+            $scope.mateMatched.$sync();
             $scope.mateAcc = $goKey('accounts/' + $scope.mateId + '/matches/' + room + '/mateId');
             $scope.mateAcc.$sync();
-            $scope.mateAcc.$on("ready", function() {
-              $scope.mateAcc.$set(user.id).then(function() {
-                document.location.href = "/chat?room=" + room;
+            $scope.userAcc.$on("ready", function() {
+              $scope.mateAcc.$on("ready", function() {
+                $scope.mateMatched.$on("ready", function() {
+                  $scope.mateMatched.$set(room).then(function() {
+                    $scope.userAcc.$set($scope.mateId).then(function() {
+                      $scope.mateAcc.$set(user.id).then(function() {
+                        document.location.href = "/chat?room=" + room;
+                      });
+                    });
+                  });
+                });
+
               });
+              console.log("Set")
             });
 
-          });
-          console.log("Set")
+          }
         });
 
       });
